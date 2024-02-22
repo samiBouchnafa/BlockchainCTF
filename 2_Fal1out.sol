27f886d76b3112cd496da59167c7c196fd27b595
samiBouchnafa <sami.bouchnafa2001@gmail.com>
2024-02-22 22:34:27 +0000

    moving the files to a folder for better view

--- 2_Fal1out.sol
+++
@@ -1,5 +0,0 @@
-/*
-This one was much simpler than fallback contract .
-the goal is the same , claim ownership of the contract .
-there is a function called Fal1out , if you call it and send ether , you simply become the owner .
-*/ 
